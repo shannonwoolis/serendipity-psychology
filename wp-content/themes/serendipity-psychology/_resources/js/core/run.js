8d3fbea6c17c6463a3f73326bb52eee3c6565e63
Shannon Woolis <shannon.woolis@adtrak.co.uk>
2024-03-24 10:04:15 +0000

    fix: some mobile changes"

--- wp-content/themes/serendipity-psychology/_resources/js/core/run.js
+++ wp-content/themes/serendipity-psychology/_resources/js/core/run.js
@@ -5,6 +5,14 @@
 	// add all of your code within here, not above or below
 	$(function () {
 
+		// Mobile bottom bar
+		$(window).scroll(function(){
+			if($(this).scrollTop() >= 500){
+				$('.mobile-bottom-bar').removeClass('translate-y-24');
+			} else {
+				$('.mobile-bottom-bar').addClass('translate-y-24');
+			}
+		});
 
 	});
 
